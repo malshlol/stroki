9c6f2a70587f82bf6027fe6296fa73883fedc391
lolze <lolzek@bk.ru>
2023-02-16 18:35:27 +0300

    l

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        String uppNewName = fullName.toUpperCase();
+        System.out.println(uppNewName);
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName);
+
+    }
+}
